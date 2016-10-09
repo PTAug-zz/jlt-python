a91554e5ffccb04f7d3a0a242f0f2f1813acb467
Paul-Tristan Auguin <paultristan.auguin@gmail.com>
2016-10-09 15:28:50 -0400

    Created discrete JLT

--- jlt-python/core.py
+++ jlt-python/core.py
@@ -9,3 +9,11 @@ def jlt_basic(dataset_in,objective_dim):
 	[trans_dataset.append(np.dot(jlt,np.transpose(dataset_in[i]))) 
 		for i in range(len(dataset_in))]
 	return trans_dataset
+
+def jlt_discrete(dataset_in,objective_dim):
+	jlt=(1/math.sqrt(objective_dim))*np.random.choice([-1,1],size=(objective_dim,
+		len(dataset_in[0])))
+	trans_dataset=[]
+	[trans_dataset.append(np.dot(jlt,np.transpose(dataset_in[i]))) 
+		for i in range(len(dataset_in))]
+	return trans_dataset
